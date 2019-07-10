1d49ebe7f72961e456beca613af181d4d36e4644
semtex <Daniel.Kopecky@techlib.cz>
2019-07-10 17:25:07 +0200

    1. Study field was changed to list, because more fields is possible. 2. To main was added function that clean studyField and transform it to the array 3. Marsmallow_remove_required was changed in order to remove validators as well and tree traversal was fixed (even nested validators are removed)

--- invenio_nusl_common/marshmallow_utils.py
+++ invenio_nusl_common/marshmallow_utils.py
@@ -1,13 +1,31 @@
-import copy
-
-
 def marshmallow_remove_required(schema):
-    for fld in getattr(schema, '_declared_fields', {}).values():
-        if fld.required:
-            fld.required = False
-        fld.validators.clear()
-        if hasattr(fld, 'container'):
-            marshmallow_remove_required(fld.container)
-        if hasattr(fld, 'nested'):
-            marshmallow_remove_required(fld.nested)
+    if hasattr(schema, "required"):
+        schema.required = False
+    if hasattr(schema, "validate"):
+        setattr(schema, "validate", None)
+    if hasattr(schema, "validators"):
+        schema.validators.clear()
+    if hasattr(schema, "_declared_fields"):
+        for fld in getattr(schema, '_declared_fields', {}).values():
+            marshmallow_remove_required(fld)
+    elif hasattr(schema, "container"):
+            marshmallow_remove_required(getattr(schema, 'container', {}))
+    elif hasattr(schema, "nested"):
+        marshmallow_remove_required(getattr(schema, 'nested', {}))
+
     return schema
+
+#    if hasattr(schema, "_declared_fields"):
+#        for fld in getattr(schema, '_declared_fields', {}).values():
+#            inside()
+#    elif hasattr(schema, "container"):
+#        for fld in getattr(schema, 'container', {}).values():
+#            inside()
+#    elif hasattr(schema, "nested"):
+#        for fld in getattr(schema, 'nested', {}).values():
+#            inside()
+#
+# # if hasattr(fld, 'container'):
+#    #     marshmallow_remove_required(fld.container)
+#    # if hasattr(fld, 'nested'):
+#    #     marshmallow_remove_required(fld.nested)
