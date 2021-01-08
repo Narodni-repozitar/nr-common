2fcc260ea281c0afce9ecbca8b50614869992cd1
Daniel Kopecký <kopecky.d@gmail.com>
2021-01-08 17:44:06 +0100

    Edited push script

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,20 +3,26 @@
 TARGET=$1
 
 setup_git() {
+  echo "SETUP GIT"
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
 commit_website_files() {
+  echo "COMMIT_WEBSITE_FILES"
   git add -A
-  git commit --message "Travis build: $GITHUB_RUN_NUMBER"
+  git commit --message "Github build: $GITHUB_RUN_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-pages "https://${GH_TOKEN}@github.com/Narodni-repozitar/nr-schemas.git"
+  echo "UPLOAD_FILES"
+  Address="https://${GH_TOKEN}@github.com/Narodni-repozitar/nr-schemas.git"
+  echo Address
+  git remote add origin-pages $Address
   git push --set-upstream origin-pages
 }
 
+echo "Target: $TARGET"
 cd $TARGET
 setup_git
 commit_website_files
