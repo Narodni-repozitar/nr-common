3439981957382ba07f439fee86b5966524e5e415
Daniel Kopecký <kopecky.d@gmail.com>
2021-04-12 14:52:40 +0200

    Implemented oarepo-search

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,11 +1,14 @@
-import json
+import functools
 
 from flask import request
+from flask.views import View
 from flask_login import login_user
 from flask_principal import AnonymousIdentity
 from flask_security import AnonymousUser
+from invenio_records_rest.query import default_search_factory
+from oarepo_search.query_parsers import query_parser
 
-from nr_common.search import NRRecordsSearch
+from nr_common.search import NRRecordsSearch, community_search_factory
 from tests.helpers import set_identity
 
 
@@ -57,3 +60,31 @@ def test_search_meta(app, db, community):
                     ]
                 }
             }
+
+
+def test_search_class(app, client):
+    def my_search_factory(*args, **kwargs):
+        my_query_parser = functools.partial(query_parser, index_name="test-index",
+                                            endpoint_name="test-endpoint")
+
+        return default_search_factory(*args,
+                                      query_parser=my_query_parser, **kwargs)
+
+    view = View()
+    with app.test_request_context(
+            '/path/2017?q=bla'):
+        # request.__setattr__("view_args", {"community_id": 1})
+        # setattr(request, "endpoint", "invenio_records_rest.draft-nresults-community_list")
+        search_obj = NRRecordsSearch()
+        search = search_obj.with_preference_param().params(version=True)
+        setattr(search, '_original_index', ['test_index'])
+        search, qs_kwargs = my_search_factory(view, search)
+    assert search.to_dict() == {
+        'query': {
+            'bool': {
+                'minimum_should_match': '0<1',
+                'filter': [{'term': {'_administration.state': 'published'}}],
+                'must': [{'query_string': {'query': 'bla'}}]
+            }
+        }, '_source': []
+    }
